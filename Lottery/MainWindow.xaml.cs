ec811e2f13ef90c13f198609ad28ed43d9cf139d
hyfree-msi <wangxianqiangcn@foxmail.com>
2021-01-17 17:55:51 +0800

    添加项目文件。

---
+++ Lottery/MainWindow.xaml.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Windows;
+
+namespace Lottery
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private volatile bool startFlag = false;
+        private volatile List<string> peoples = new List<string>();
+        private volatile Thread thread = null;
+        private volatile string prizeName = string.Empty;
+        private volatile int prizeNameIndex = 0;
+        private bool firstInit = false;
+
+        //抽奖结果
+        private volatile Dictionary<string, string> keys = new Dictionary<string, string>();
+
+        private Object lockObj = new Object();
+        private volatile int firstPrizeSize = 0;
+        private volatile int secondPrizeSize = 0;
+        private volatile int thirdPrizeSize = 0;
+
+        private string firstPrizeName = string.Empty;
+        private string secondPrizeName = string.Empty;
+        private string thirdPrizeName = string.Empty;
+
+        public int FirstPrizeSize
+        {
+            get
+            {
+                lock (lockObj)
+                {
+                    return firstPrizeSize;
+                }
+            }
+            set
+            {
+                lock (lockObj)
+                {
+                    this.firstPrizeSize = value;
+                }
+            }
+        }
+
+        public int SecondPrizeSize
+        {
+            get
+            {
+                lock (lockObj)
+                {
+                    return secondPrizeSize; ;
+                }
+            }
+            set
+            {
+                lock (lockObj)
+                {
+                    this.secondPrizeSize = value;
+                }
+            }
+        }
+
+        public int ThirdPrizeSize
+        {
+            get
+            {
+                lock (lockObj)
+                {
+                    return thirdPrizeSize; ;
+                }
+            }
+            set
+            {
+                lock (lockObj)
+                {
+                    this.thirdPrizeSize = value;
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (startFlag)
+            {
+                startFlag = false;
+                DoStopAsync();
+                this.StartButton.Content = "开始";
+                SelectPrizeComboBox.IsEnabled = true;
+            }
+            else
+            {
+                var result = DoCheck();
+                if (!result)
+                {
+                    return;
+                }
+                DoStart();
+                startFlag = true;
+                SelectPrizeComboBox.IsEnabled = false;
+
+                this.StartButton.Content = "停止";
+            }
+        }
+
+        public bool DoCheck()
+        {
+            //检查人员是否设置
+            if (peoples.Count == 0)
+            {
+                MessageBox.Show("抽奖参与者=0，无法抽奖");
+                return false;
+            }
+            prizeNameIndex = SelectPrizeComboBox.SelectedIndex;
+            //检查奖品是否抽完了
+            switch (SelectPrizeComboBox.SelectedIndex)
+            {
+                case 0:
+                    if (FirstPrizeSize < 1)
+                    {
+                        MessageBox.Show("一等奖已经被抽完了");
+                        return false;
+                    }
+                    prizeName = FirstPrizeNameTextBox.Text;
+                    break;
+
+                case 1:
+                    if (SecondPrizeSize < 1)
+                    {
+                        MessageBox.Show("二等奖已经被抽完了");
+                        return false;
+                    }
+                    prizeName = SecondPrizeNameTextBox.Text;
+                    break;
+
+                case 2:
+                    if (ThirdPrizeSize < 1)
+                    {
+                        MessageBox.Show("三等奖已经被抽完了");
+                        return false;
+                    }
+                    prizeName = ThirdPrizeNameTextBox.Text;
+                    break;
+
+                default:
+                    break;
+            }
+            return true;
+        }
+
+        /// <summary>
+        ///  开始循环抽奖
+        /// </summary>
+        private bool DoStart()
+        {
+            //开始抽奖 开启一个线程循环显示名字
+            thread = new Thread(CycleShow);
+            thread.Start();
+            return true;
+        }
+
+        /// <summary>
+        /// 循环显示名字
+        /// </summary>
+        public void CycleShow()
+        {
+            while (startFlag)
+            {
+                try
+                {
+                    foreach (string item in peoples)
+                    {
+                        if (startFlag)
+                        {
+                            SetPeopleName(item);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void DoSet(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //设置参与人数
+                InitPeople();
+                //设置抽奖奖品
+                InitPrize();
+                MessageBox.Show($"设置成功，参与抽奖人数为:{peoples.Count}");
+                firstInit = true;
+                this.StartButton.Content = "开始";
+                this.StartButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("初始化抽奖参数失败，请检查抽奖参数");
+            }
+        }
+
+        /// <summary>
+        /// 初始化抽奖人
+        /// </summary>
+        public bool InitPeople()
+        {
+            peoples.Clear();
+            string text = this.PeoplesTextBox.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                MessageBox.Show("抽奖人名单填写错误");
+                return false;
+            }
+            else
+            {
+                string[] array = text.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                foreach (var item in array)
+                {
+                    if (!string.IsNullOrEmpty(item))
+                    {
+                        if (peoples.Contains(item))
+                        {
+                            MessageBox.Show("抽奖人名单冲突：" + item);
+                            return false;
+                        }
+                        else
+                        {
+                            peoples.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public void InitPrize()
+        {
+            firstPrizeName = this.FirstPrizeNameTextBox.Text;
+            string p1SizeText = this.FirstPrizeSizeTextBox.Text;
+            firstPrizeSize = Convert.ToInt32(p1SizeText);
+            this.P1.Content=$"一等奖剩余数量：{p1SizeText}";
+
+            secondPrizeName = this.SecondPrizeNameTextBox.Text;
+            string p2SizeText = this.SecondPrizeSizeTextBox.Text;
+            SecondPrizeSize = Convert.ToInt32(p2SizeText);
+                this.P2.Content=$"二等奖剩余数量：{p2SizeText}";
+
+
+            thirdPrizeName = this.ThirdPrizeNameTextBox.Text;
+            string p3SizeText = this.ThirdPrizeSizeTextBox.Text;
+            thirdPrizeSize = Convert.ToInt32(p3SizeText);
+             this.P3.Content=$"三等奖剩余数量：{p3SizeText}";
+        }
+
+        public void SetPeopleName(string tempPeopleName, bool needCheck = true)
+        {
+            Console.WriteLine(tempPeopleName);
+            this.PeopleNameTextBlock.Dispatcher.Invoke(() =>
+                  {
+                      if (needCheck && !startFlag)
+                      {
+                          return;
+                      }
+                      this.PeopleNameTextBlock.Text = tempPeopleName;
+                  });
+        }
+
+        private void DoStopAsync()
+        {
+            lock (lockObj)
+            {
+                if (thread != null)
+                {
+                    thread.Abort();
+                }
+            }
+
+            //使用RNGCryptoServiceProvider产生一个随机性足够好的随机数
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            SecureRandom random = new SecureRandom(rngCsp);
+             int index=0;
+            if (peoples.Count!=1)
+            {
+                index = random.NextInt(0, peoples.Count - 1);
+            }
+            string tempPeopleName = peoples[index];
+            SetPeopleName(tempPeopleName, false);
+
+            string log = $"恭喜{tempPeopleName}获得{prizeName}";
+            File.AppendAllText("log.txt",log);
+            this.logTextBox.AppendText($"{log}\r\n");
+            Console.WriteLine(log);
+            MessageBox.Show(log);
+
+            //扣除奖项
+            peoples.RemoveAt(index);
+            switch (prizeNameIndex)
+            {
+                case 0:
+                    firstPrizeSize--;
+                    this.P1.Content=$"一等奖剩余数量：{firstPrizeSize}";
+                    break;
+
+                case 1:
+                    secondPrizeSize--;
+                    this.P2.Content=$"二等奖剩余数量：{secondPrizeSize}";
+                    break;
+
+                case 2:
+                    thirdPrizeSize--;
+                    this.P3.Content=$"三等奖剩余数量：{thirdPrizeSize}";
+                    break;
+                default:
+                    break;
+            }
+            //释放控件
+            SelectPrizeComboBox.IsEnabled = true;
+
+        }
+    }
+}
---
+++ Lottery/SecureRandom.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+   public class SecureRandom
+    {
+        private readonly static double DOUBLE_UNIT = 1.0 / (1L << 53);
+        RandomNumberGenerator generator;
+        public SecureRandom(RandomNumberGenerator generator)
+        {
+            this.generator = generator;
+        }
+        //产生bits位的随机整数
+        // bits>=1
+        protected uint Next(int bits)
+        {
+            byte[] bs = new byte[4];
+            generator.GetBytes(bs);
+            uint x = BitConverter.ToUInt32(bs, 0);
+            return x >> (32 - bits);
+        }
+        //产生bits位的随机整数
+        //bits>=1
+        protected ulong Next2(int bits)
+        {
+            byte[] bs = new byte[8];
+            generator.GetBytes(bs);
+            ulong x = BitConverter.ToUInt64(bs, 0);
+            return x >> (64 - bits);
+        }
+ 
+ 
+        protected int BitLength(int x)
+        {
+            int len = 0;
+            while (x > 0)
+            {
+                len++;
+                x >>= 1;
+            }
+            return len;
+        }
+        protected int BitLength(long x)
+        {
+            int len = 0;
+            while (x > 0)
+            {
+                len++;
+                x >>= 1;
+            }
+            return len;
+        }
+ 
+        public int NextInt()
+        {
+            return (int)Next(32);
+        }
+        //max>=1,不包括max
+        public int NextInt(int max)
+        {
+            if (max <= 0) throw new ArgumentException("max <= 0");
+            int len = BitLength(max);
+            uint x = Next(len);
+            while (x >= max)
+            {
+                x = Next(len);
+            }
+            return (int)x;
+        }
+        //不包括max
+        public int NextInt(int min, int max)
+        {
+            return NextInt(max - min) + min;
+        }
+        //max>=1,不包括max
+        public int FastNextInt(int max)
+        {
+            byte[] bs = new byte[4];
+            generator.GetBytes(bs);
+            int value = BitConverter.ToInt32(bs, 0);
+            value = value % max;
+            if (value < 0) value = -value;
+            return value;
+        }
+        //不包括max
+        public int FastNextIntint(int min, int max)
+        {
+            return FastNextInt(max - min) + min;
+        }
+        public long NextLong()
+        {
+            return ((long)(Next(32)) << 32) + Next(32);
+        }
+        //max>=1,不包括max
+        public long NextLong(long max)
+        {
+            if (max <= 0) throw new ArgumentException("max <= 0");
+            int len = BitLength(max);
+            ulong x = Next2(len);
+            while (x >= (ulong)max)
+            {
+                x = Next2(len);
+            }
+            return (long)x;
+        }
+        //不包括max
+        public long NextLong(long min, long max)
+        {
+            return NextLong(max - min) + min;
+        }
+        //max>=1,不包括max
+        public long FastNextLong(long max)
+        {
+            byte[] bs = new byte[8];
+            generator.GetBytes(bs);
+            long value = BitConverter.ToInt64(bs, 0);
+            value = value % max;
+            if (value < 0) value = -value;
+            return value;
+        }
+        //不包括max
+        public long FastNextLong(long min, long max)
+        {
+            return FastNextLong(max - min) + min;
+        }
+        public bool NextBoolean()
+        {
+            return Next(1) != 0;
+        }
+        //此方法是java源码中拷贝，此算法得到的浮点数的随机性未考究
+        public float NextFloat()
+        {
+            return Next(24) / ((float)(1 << 24));
+        }
+        //此方法是java源码中拷贝，此算法得到的浮点数的随机性未考究
+        public double NextDouble()
+        {
+            return (((long)(Next(26)) << 27) + Next(27)) * DOUBLE_UNIT;
+        }
+ 
+        //数组长度需要一致
+        protected bool GreaterThanOrEqual(byte[] a, byte[] b)
+        {
+            //if (a.Length > b.Length) return true;
+            //if (a.Length < b.Length) return false;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > b[i]) return true;
+                if (a[i] < b[i]) return false;
+            }
+            return true;//全等
+        }
+ 
+        protected byte[] Next3(int bits)
+        {
+            int len = (bits + 7) >> 3;
+            byte[] bs = new byte[len];
+            generator.GetBytes(bs);
+            bs[0] >>= (8 - bits) & 0x7;
+            return bs;
+        }
+        protected int BitLength(byte[] x)
+        {
+            int i = 0;
+            for (; i < x.Length && x[i] == 0; i++) ;
+            return i == 8 ? 0 : BitLength(x[i]) + (x.Length - i - 1) * 8;
+        }
+ 
+        public byte[] NextBigInt(byte[] max)
+        {
+            int bitLen = BitLength(max);
+            if(bitLen==0) throw new ArgumentException("max == 0");
+            byte[] max_ = new byte[(bitLen + 7) >>3];
+            Array.Copy(max, max.Length - max_.Length, max_, 0, max_.Length);
+            byte[] x = Next3(bitLen);
+            while (GreaterThanOrEqual(x,max_))
+            {
+                x = Next3(bitLen);
+            }
+            return x;
+        }
+    }
+
+}
